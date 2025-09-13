6f015d882133ce5be3bff88a482f74eb0e542b5a
SweetySweetingtonsSweets <sweetysweetington@gmail.com>
2025-09-13 18:58:54 -0400

    Update willy_wonka_candies_willy_category.js

--- my_folder/willy_wonka_candies_willy_category.js
+++ my_folder/willy_wonka_candies_willy_category.js
@@ -1,1 +1,10 @@
-
+elements.TestingElement = {
+    color: "#8B4513",
+    behavior: [
+        ¨XX|XX|XX¨,
+        ¨XX|XX|XX¨,
+        ¨XX|CR:Wet_Sand50%|XX¨,
+    ]
+    category: "Testing",
+    state: "solid",
+  
