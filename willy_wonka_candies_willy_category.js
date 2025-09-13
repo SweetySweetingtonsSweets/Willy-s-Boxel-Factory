684e407bb5ce5a30b0de301dfdcf3989d96bcd25
SweetySweetingtonsSweets <sweetysweetington@gmail.com>
2025-09-13 19:01:16 -0400

    Delete willy_wonka_candies_willy_category.js

--- willy_wonka_candies_willy_category.js
+++
@@ -1,9 +0,0 @@
-elements.TestingElement = {
-    color: "#8B4513",
-    behavior: [
-        ¨XX|XX|XX¨,
-        ¨XX|XX|XX¨,
-        ¨XX|CR:Wet_Sand50%|XX¨,
-    ]
-    category: "Testing",
-    state: "solid",
